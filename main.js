99f12356c0c49443ccfd9aa72e79bb5f777ba014
Banjo <jbdinglasan@webfirmament.com>
2023-10-26 17:56:06 +0800

    add popup

--- main.js
+++ main.js
@@ -11,3 +11,32 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     });
 });
+document.addEventListener('DOMContentLoaded', function() {
+    const popupContainer = document.getElementById('popup-container');
+    const closeButton = document.getElementById('close-button');
+    const showPopupButton = document.getElementById('show-popup-button');
+
+    // Function to show the popup
+    function showPopup() {
+        popupContainer.style.display = 'block';
+    }
+
+    // Function to close the popup
+    function closePopup() {
+        popupContainer.style.display = 'none';
+    }
+
+    // Show the popup when the button is clicked
+    showPopupButton.addEventListener('click', showPopup);
+
+    // Close the popup when the close button is clicked
+    closeButton.addEventListener('click', closePopup);
+
+    // Close the popup when the user clicks anywhere outside the content
+    window.addEventListener('click', function(event) {
+        if (event.target === popupContainer) {
+            closePopup();
+        }
+    });
+});
+
